222c0189604f5fe30cc1e0409159f04c2144f4a2
mineclover <mineclover@naver.com>
2022-08-31 17:41:40 +0900

    js 구조잡기 초안 + canvas 자료조사

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function minusSize(num){
   mainSzie -= num;
   root.style.setProperty(`--main-size`,`${mainSzie}px`);
 }
+
+
+function columnSetting(num) {
+  let selectObj = document.querySelector('.c');
+  selectObj.innerHTML = '';
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('col');
+  selectObj.append(newDiv);
+  
+  for(let i = 0;i <num; i++){
+    document
+  }
+}
+
+//canvas 
+//https://goodsgoods.tistory.com/268
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+//https://stackoverflow.com/questions/18316065/set-quality-of-png-with-html2canvas
