1b63cf265a170e09e3dc33918a11b7e6dffc60e7
mineclover <mineclover@naver.com>
2022-09-05 17:53:04 +0900

    크기 입력 방식 변경

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ var mainSzie = 9;
 function plusSize(num){
   let root = document.documentElement;
   mainSzie += num;
+  if(mainSzie < 2) {mainSzie = 2;};
   root.style.setProperty(`--main-size`,`${mainSzie}px`);
 }
 function minusSize(num){
   let root = document.documentElement;
-  let data = root.style.getPropertyValue(`--main-size`);
+  //let data = root.style.getPropertyValue(`--main-size`);
   mainSzie -= num;
+  if(mainSzie < 2) {mainSzie = 2;};
+  root.style.setProperty(`--main-size`,`${mainSzie}px`);
+}
+
+function viewSize(){
+  let root = document.documentElement;
+  let i = document.querySelector('#viewSizeControll').value;
+  mainSzie = i;
+  
   root.style.setProperty(`--main-size`,`${mainSzie}px`);
 }
 
@@ -28,3 +38,5 @@ function columnSetting(num) {
 //https://goodsgoods.tistory.com/268
 //https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
 //https://stackoverflow.com/questions/18316065/set-quality-of-png-with-html2canvas
+
+
